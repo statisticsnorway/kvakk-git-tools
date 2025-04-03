e6964b16a8a367a3e8e6983aee291c754af928ee
skykanin <3789764+skykanin@users.noreply.github.com>
2025-04-03 15:12:39 +0200

    fix formatting

--- kvakk_git_tools/validate_ssb_local_git_files.py
+++ kvakk_git_tools/validate_ssb_local_git_files.py
@@ -3,14 +3,16 @@
 import sys
 from pathlib import Path
 
+
 def _read_local_git_file(gitignore_path: Path) -> list[str]:
-  lines = []
-  with open(gitignore_path, 'r', encoding='UTF-8') as file:
-    while line := file.readline():
-      stripped_line = line.rstrip()
-      if not stripped_line.startswith('#'):
-          lines.append(stripped_line)
-  return lines
+    lines = []
+    with open(gitignore_path, "r", encoding="UTF-8") as file:
+        while line := file.readline():
+            stripped_line = line.rstrip()
+            if not stripped_line.startswith("#"):
+                lines.append(stripped_line)
+    return lines
+
 
 def validate_local_git_files(cwd: Path = Path()) -> bool:
     """Validate the local Git files.
@@ -24,6 +26,7 @@ def validate_local_git_files(cwd: Path = Path()) -> bool:
     gitignore_path = cwd.joinpath(".gitignore")
     gitattributes_path = cwd.joinpath(".gitattributes")
     return _validate_local_git_files(gitignore_path, gitattributes_path)
+
 
 def _validate_local_git_files(gitignore_path: Path, gitattributes_path: Path) -> bool:
     """Validates local Git files (.gitattributes and .gitignore) against recommended versions.
@@ -42,20 +45,24 @@ def _validate_local_git_files(gitignore_path: Path, gitattributes_path: Path) ->
 
     if sys.version_info.major == 3 and sys.version_info.minor < 10:
         import pkg_resources
+
         relative_path = pkg_resources.resource_stream(__package__)
     else:
         from importlib.resources import files
+
         relative_path = files(__package__)
 
     ssb_recommended_ignore_file_path = relative_path.joinpath("recommended/gitignore")
-    ssb_recommended_attributes_file_path = relative_path.joinpath("recommended/gitattributes")
+    ssb_recommended_attributes_file_path = relative_path.joinpath(
+        "recommended/gitattributes"
+    )
     ssb_recommended_ignore = _read_local_git_file(ssb_recommended_ignore_file_path)
-    ssb_recommended_attributes = _read_local_git_file(ssb_recommended_attributes_file_path)
+    ssb_recommended_attributes = _read_local_git_file(
+        ssb_recommended_attributes_file_path
+    )
     local_ignore = _read_local_git_file(gitignore_path)
     local_attributes = _read_local_git_file(gitattributes_path)
 
-    return all(
-        line in local_ignore for line in ssb_recommended_ignore
-    ) and all(
+    return all(line in local_ignore for line in ssb_recommended_ignore) and all(
         line in local_attributes for line in ssb_recommended_attributes
     )
--- tests/test_validate_local_git_files.py
+++ tests/test_validate_local_git_files.py
@@ -3,6 +3,7 @@
 from kvakk_git_tools.validate_ssb_local_git_files import _validate_local_git_files
 from pathlib import Path
 
+
 def test_validate_git_config_equal() -> None:
     """Test the functionality of the `validate_gitignore()` function."""
     gitignore_path = Path("./tests/test_files/gitignore.test")
