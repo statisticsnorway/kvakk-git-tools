6952e3ec32a16daa437fd12889dbc3eece860034
skykanin <3789764+skykanin@users.noreply.github.com>
2025-04-03 14:46:58 +0200

    add validation logic for git files
    meaning the .gitignore and .gitattributes files

---
+++ tests/test_validate_local_git_files.py
@@ -0,0 +1,11 @@
+"""Test module for validate_ssb_gitignore."""
+
+from kvakk_git_tools.validate_ssb_local_git_files import _validate_local_git_files
+from pathlib import Path
+
+def test_validate_git_config_equal() -> None:
+    """Test the functionality of the `validate_gitignore()` function."""
+    gitignore_path = Path("./tests/test_files/gitignore.test")
+    gitattributes_path = Path("./tests/test_files/gitattributes.test")
+
+    assert _validate_local_git_files(gitignore_path, gitattributes_path)
