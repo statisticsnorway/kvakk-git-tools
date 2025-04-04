e5c7703d80794a1863e2e865bb6e17fa7a678464
skykanin <3789764+skykanin@users.noreply.github.com>
2025-04-04 13:10:43 +0200

    Bump version: 2.5.0 → 2.5.1

--- kvakk_git_tools/ssb_gitconfig.py
+++ kvakk_git_tools/ssb_gitconfig.py
@@ -9,7 +9,7 @@ If there is an existing .gitconfig file, it is backed up, and the name and email
 address are extracted from it and reused.
 """
 
-__version__ = "2.5.0"
+__version__ = "2.5.1"
 
 import argparse
 import getpass
